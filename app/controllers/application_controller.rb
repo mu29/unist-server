bff8282ad8ce98f72202109480c1406f945952bd
InJung Chung <mu29gl@gmail.com>
2016-09-17 00:30:40 +0900

    success, error 메시지 렌더링

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,25 @@ class ApplicationController < ActionController::API
   attr_reader :current_user
   before_action :authenticate_request
 
+  protected
+
+  def render_success(message = '', options = {}, status = :ok)
+    render status: status,
+           json: {
+             success: true,
+             message: message
+           }.merge(options)
+  end
+
+  def render_error(message, status = :unprocessable_entity)
+    logger.error(message)
+    render status: status,
+           json: {
+             success: false,
+             message: message
+           }
+  end
+
   private
 
   def authenticate_request
