9266b11a718d5fdca1e54cdd1d96c19b7d704d03
InJung Chung <mu29gl@gmail.com>
2016-09-17 00:20:38 +0900

    로그인 하는 `AuthenticateUser` Interactor

---
+++ app/interactors/authenticate_user.rb
@@ -0,0 +1,18 @@
+class AuthenticateUser
+  include Interactor
+
+  def call
+    JsonWebToken.encode(user_id: user.id)
+  end
+
+  private
+
+  def user
+    user = User.find_by(email: context.email)
+    if user && user.authenticate(context.password)
+      user
+    else
+      context.fail!(error: '아이디와 비밀번호를 확인해주세요.')
+    end
+  end
+end
