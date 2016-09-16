8ffef126ba0962623bed106d9a03fe31f40cfd5e
InJung Chung <mu29gl@gmail.com>
2016-09-17 00:30:23 +0900

    매 요청마다 로그인 해보기

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).user
+    render_error 'Unauthorized', :unauthorized unless @current_user
+  end
 end
