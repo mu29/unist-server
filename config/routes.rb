b5b357435ed6e2cfc278484aa606cdbb8db2da5c
InJung Chung <mu29gl@gmail.com>
2016-09-17 16:44:57 +0900

    메일 인증을 위해 라우터 수정

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create, :destroy]
-  resources :users, only: [:create]
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
 end
