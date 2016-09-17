e74ca9286c45e0eb8522e759acbe8b3b573cacc3
InJung Chung <mu29gl@gmail.com>
2016-09-18 00:27:07 +0900

    `ApplicatoinController#verify_authenticate_token` 에 header 정확하게 넘기도록 수정

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::API
   private
 
   def verify_authenticate_token
-    @current_user = AuthorizeApiRequest.call(request.headers).user
+    @current_user = AuthorizeApiRequest.call(headers: request.headers).user
     render_error 'Unauthorized', :unauthorized unless @current_user
   end
 end
