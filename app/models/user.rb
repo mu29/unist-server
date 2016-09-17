44bf3109f343e56b88b3953868245f16a6b18a11
InJung Chung <mu29gl@gmail.com>
2016-09-17 16:29:36 +0900

    유저 생성될 때 confirm_token 생성하기

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   validates :email, :name, uniqueness: true
   validates_presence_of :email, :password_digest, :name
+
+  before_create :generate_confirm_token
+
+  private
+
+  def generate_confirm_token
+    self.confirm_token = SecureRandom.urlsafe_base64.to_s
+  end
 end
