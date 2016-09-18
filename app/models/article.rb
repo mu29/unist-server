bff27cd5616dfca97425b31cc93ac8cb74f44e20
InJung Chung <mu29gl@gmail.com>
2016-09-18 12:34:06 +0900

    `Article` 모델에 `category` 태그 설정

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,14 @@
 class Article < ApplicationRecord
   include Authority::Abilities
 
+  acts_as_taggable_on :categories
+
   belongs_to :user
   has_many :comments
 
   validates_presence_of :title, :content
+
+  scope :with_category, lambda { |category|
+    tagged_with(category, on: :categories) if category.present?
+  }
 end
