1e97c168b098f4df42d0efc77b249a05b2174002
InJung Chung <mu29gl@gmail.com>
2016-09-18 12:01:40 +0900

    `Category` 모델 삭제

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,6 @@ class Article < ApplicationRecord
 
   belongs_to :user
   has_many :comments
-  has_and_belongs_to_many :categories
 
   validates_presence_of :title, :content
 end
--- app/models/category.rb
+++
@@ -1,5 +0,0 @@
-class Category < ApplicationRecord
-  has_and_belongs_to_many :articles
-
-  validates_presence_of :name
-end
